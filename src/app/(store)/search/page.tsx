4b47111351dc9a6a550dca320a1a3c5aae7bd07b
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-11-11 21:47:29 -0300

    feat: add Search page

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,3 +1,79 @@
-export default function Search() {
-  return <h1>Hello World</h1>
+import Link from 'next/link'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
+
+import { api } from '@/data'
+import { Product } from '@/data/types'
+import { getFormattedCurrency } from '@/helpers'
+
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60 // 1 hour
+    }
+  })
+  const products = await response.json()
+
+  return products
+}
+
+export default async function Search({ searchParams }: SearchProps) {
+  const { q: query } = searchParams
+
+  if (!query) {
+    redirect('/')
+  }
+
+  const products = await searchProducts(query)
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        {products.map(product => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.slug}`}
+              className={`
+            group relative rounded-lg overflow-hidden
+            flex justify-center items-end bg-zinc-900
+          `}
+            >
+              <Image
+                src={product.image}
+                width={480}
+                height={480}
+                quality={100}
+                alt='Product Image'
+                className='group-hover:scale-105 transition-transform duration-500'
+              />
+
+              <div className={`
+              absolute bottom-10 rigth-10 h-12 flex items-center gap-2 max-w-[280px]
+              rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5
+            `}>
+                <span className='text-sm truncate'>{product.title}</span>
+                <span className={`
+                flex h-full items-center justify-center rounded-full
+                bg-violet-500 px-4 font-semibold'
+              `}>
+                  {getFormattedCurrency(product.price, true)}
+                </span>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
